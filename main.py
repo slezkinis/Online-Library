a8a0720df3ac83dca067ab502a7c09077fc057e7
slezkinis <slezkin.is@mail.ru>
2022-08-29 16:35:22 +0300

    Clear up code

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from pathvalidate import sanitize_filename
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin, unquote, urlsplit
+from pprint import pprint
 
 
 def check_for_redirect(response):
@@ -28,50 +29,35 @@ def download_image(url, directory='images/'):
     Path(directory).mkdir(parents=True, exist_ok=True)
     with open(os.path.join(directory, filename), 'wb') as file:
         file.write(response.content)
+    
 
-
-def download_comments(id):
-    url = f'https://tululu.org/b{id}/'
-    response = requests.get(url)
-    response.raise_for_status()
-    soup = BeautifulSoup(response.text, 'lxml')
+def parse_book_page(soup):
+    about_book = {}
     comments_tags = soup.find_all('div', class_='texts')
     comments_texts = []
-    if comments_tags == []:
-        return ''
     for comment_tag in comments_tags:
         comment_text_tag = comment_tag.find('span', class_='black')
         comments_texts.append(comment_text_tag.text)
-    return comments_texts
+    about_book['comments'] = comments_texts
 
-
-
-def get_book_info(id):
-    url = f'https://tululu.org/b{id}/'
-    response = requests.get(url)
-    response.raise_for_status()
-    soup = BeautifulSoup(response.text, 'lxml')
     title_tag = soup.find('h1')
     book_image = soup.find('div', class_='bookimage').find('img')['src']
     title_text = title_tag.text
     (book_title, book_author) = title_text.split('::')
-    return f'{id}. {book_title.strip()}.txt', urljoin('https://tululu.org', book_image)
+    about_book['book_title'] = book_title
+    about_book['book_author'] = book_author
+    about_book['book_image'] = urljoin('https://tululu.org', book_image)
 
-
-def get_books_genres(id):
-    url = f'https://tululu.org/b{id}/'
-    response = requests.get(url)
-    response.raise_for_status()
-    soup = BeautifulSoup(response.text, 'lxml')
     genre_field = soup.find('span', class_='d_book')
     genres_tags = genre_field.find_all('a')
     genres_texts = []
     for genre_tag in genres_tags:
         genre_text = genre_tag.text
         genres_texts.append(genre_text)
-    return genres_texts
+    about_book['book_genres'] = genres_texts
+    return about_book
 
-        
+     
 if __name__ == '__main__':
     directry = 'books'
     for book_id in range(1, 11):
@@ -81,12 +67,16 @@ if __name__ == '__main__':
         try:
             response.raise_for_status()
             check_for_redirect(response)
-            (filename, book_image_url) = get_book_info(book_id)
+            url = f'https://tululu.org/b{book_id}/'
+            response = requests.get(url)
+            response.raise_for_status()
+            soup = BeautifulSoup(response.text, 'lxml')
+            about_book = parse_book_page(soup)
             # download_txt(response, filename, directry)
             # download_image(book_image_url)
             # comments = download_comments(book_id)
-            print(filename)
-            print(get_books_genres(book_id))
+            print(book_id)
+            pprint(about_book)
             # for comment in comments:
             #     print(comment)
         except requests.exceptions.HTTPError:
