4f756801e758335aafea66eee57399730b9ec209
slezkinis <slezkin.is@mail.ru>
2022-09-13 10:49:54 +0300

    Add selector

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -16,38 +16,42 @@ if __name__ == '__main__':
         response = requests.get(url)
         response.raise_for_status()
         soup = BeautifulSoup(response.text, 'lxml')
-        books_url_parts = soup.find_all('table', class_='d_book')
+        book_selector = 'body .d_book'
+        books_url_parts = soup.select(book_selector)
         for url_part in books_url_parts:
             try:
+                url_selector = 'a'
                 book_url = urljoin(
                     url,
-                    url_part.find('a')['href']
+                    url_part.select_one(url_selector)['href']
                 )
-                book_id = (url_part.find('a')['href']).replace('/', '').replace('b', '')
+                book_id = (url_part.select_one(url_selector)['href']).replace('/', '').replace('b', '')
                 book_response = requests.get(book_url)
                 check_for_redirect(book_response)
                 book_soup = BeautifulSoup(book_response.text, 'lxml')
-                title_tag = book_soup.find('h1')
-                book_image = book_soup.find('div', class_='bookimage').find('img')['src']
+                title_selector = 'h1'
+                title_tag = book_soup.select_one(title_selector)
+                img_selector = '.bookimage img'
+                book_image = book_soup.select_one(img_selector)['src']
                 title_text = title_tag.text
                 book_title, book_author = title_text.split('::')
+                book_title = book_title.strip()
                 
                 url = 'https://tululu.org/txt.php'
                 params = {'id': book_id}
                 download_response = requests.get(url, params=params)
                 download_response.raise_for_status()
-                download_txt(download_response, f'{book_title}.txt'.strip(), 'books')
-                book_path = join('books', f'{book_title.strip()}.txt')
+                file_name = f'{book_title}.txt'
+                download_txt(download_response, file_name, 'books')
+                book_path = join('books', file_name)
                 image_path = download_image(urljoin(book_url, book_image))
-                # print(image_path)
-                # print(book_path)
-                genre_field = book_soup.find('span', class_='d_book')
-                genres_tags = genre_field.find_all('a')
+                genres_selector = 'span.d_book a'
+                genres_tags = book_soup.select(genres_selector)
                 genres_texts = [genre_tag.text for genre_tag in genres_tags]
-
-                comments_tags = book_soup.find_all('div', class_='texts')
+                comments_selector = 'div.texts span.black'
+                comments_tags = book_soup.select(comments_selector)
                 comments_texts = [
-                    comment_tag.find('span', class_='black').text
+                    comment_tag.text
                     for comment_tag in comments_tags
                 ]
                 about_book = {
@@ -58,15 +62,11 @@ if __name__ == '__main__':
                     'comments': comments_texts,
                     'genres': genres_texts
                 }
-                print(about_book)
                 about_books.append(about_book)
                 
 
             except requests.exceptions.HTTPError:
                 continue
-
-    # print(about_books)
-    # json_string = json.dumps(about_books, ensure_ascii=False).encode('utf8')
     with open("about_books.json", "w", encoding='utf8') as my_file:
         json.dump(about_books, my_file, ensure_ascii=False)
 
