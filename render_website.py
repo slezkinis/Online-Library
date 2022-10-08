7cfb1b0dc1b2b326e0038ba224ffe67e88293200
slezkinis <slezkin.is@mail.ru>
2022-10-08 19:06:41 +0300

    Split books list by pages

--- render_website.py
+++ render_website.py
@@ -2,28 +2,28 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 import json
 from livereload import Server
 from more_itertools import chunked
+from pathlib import Path
 
 
 def rebuild():
-    with open('about_books.json', 'r', encoding='utf8') as file:
-        books_json = file.read()
-    books = json.loads(books_json)
-    split_books = list(chunked(books, 2))
-
+    Path('pages').mkdir(parents=True, exist_ok=True)
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
-    )
-
+        )
     template = env.get_template('template.html')
-
-    rendered_page = template.render(
-        split_books=split_books
-    )
-
-    with open('index.html', 'w', encoding="utf8") as file:
-        file.write(rendered_page)
-    
+    with open('about_books.json', 'r', encoding='utf8') as file:
+        books_json = file.read()
+    books = json.loads(books_json)
+    pages = list(chunked(books, 10))
+    for page_number, page_books in enumerate(pages):
+        split_books = list(chunked(page_books, 10))
+        rendered_page = template.render(
+            split_books=split_books
+        )
+        with open(f'pages/index_{page_number}.html', 'w', encoding="utf8") as file:
+            file.write(rendered_page)
+        
     print("Site rebuilt")
 
 rebuild()
